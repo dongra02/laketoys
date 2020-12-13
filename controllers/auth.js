ca2a68a8432de36d6f190b9135f1cdc5dd036cdf
dongra02 <dsgraham1@gmail.com>
2020-12-13 07:48:12 -0500

    login controller written

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,8 +12,24 @@ async function register (req, res, next) {
   }
 }
 
+async function login (req, res, next) {
+  try {
+    const user  = await User.findOne({ email: req.body.email })
+    if (!user || !user.validatePassword(req.body.password)) throw new Error(unauthorized)
+    const token = jwt.sign(
+      { sub: user._id },
+      secret,
+      { expiresIn: '7 days'}
+    )
+    res.status(202).json({ message: `Welcome back to the lake, ${user.username}` , token })
+  } catch (err) {
+    next(err)
+  }
+}
+
 
 
 module.exports = {
-  register
+  register,
+  login
 }
