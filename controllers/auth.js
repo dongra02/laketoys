d4825f86108bec3567d25485d09ce023a409f8ac
dongra02 <dsgraham1@gmail.com>
2020-12-13 07:19:26 -0500

    register function written, needs test

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,19 @@
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 const { secret } = require('../config/environment')
+const { unauthorized, notFound } = require('../lib/errorMessage')
+
+async function register (req, res, next) {
+  try {
+    const user = await User.create(req.body)
+    res.status(201).json({ message: `Welcome to the lake, ${user.username}` })
+  } catch (err) {
+    next(err)
+  }
+}
+
+
+
+module.exports = {
+  register
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const { dbURI, port } = require('./config/environment')
 const logger = require('./lib/logger')
+const errorHandler = require('./lib/errorHandler')
 
 const app = express()
 
@@ -21,5 +22,7 @@ mongoose.connect(
 app.use(express.json())
 
 app.use(logger)
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`👂👂 Listening on port ${port} 👂👂`))
