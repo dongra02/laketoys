f2849adeeb4622d04a31b846e3b72daf8edc2092
dongra02 <dsgraham1@gmail.com>
2020-12-16 11:37:19 -0500

    index working, ready to begin styling and user story

--- frontend/src/components/toys/ToyIndex.js
+++ frontend/src/components/toys/ToyIndex.js
@@ -8,7 +8,22 @@ const ToyIndex = () => {
   console.log(data)
 
   return (
-    <h1>This will be Toy Index</h1>
+    <>
+    {loading && <p>currently loading</p>}
+    {error && <p>Don you need to fix this error and set up an error landinge page</p>}
+    {data && data.map(toy => {
+      // create a result list component feed toys as toys props
+      // create a list item componenent and map the toys as props (spread toy)
+      // style list item how to appear
+      // style list how to behave
+      return (
+        <>
+        <h3>{toy.name}</h3>
+        <img src={toy.images} alt='rental toy'/>
+        </>
+      )
+    })}
+    </>
   )
 }
 
