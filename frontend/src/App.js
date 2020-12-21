5a3af0c6b8a4c5cccb23f43e446d83ec7f4cb16f
dongra02 <dsgraham1@gmail.com>
2020-12-21 10:52:41 -0500

    storing user data on app state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,27 @@ import Banner from './components/common/Banner'
 import ToyIndex from './components/toys/ToyIndex'
 import ProfileShow from './components/profile/ProfileShow'
 
+import { getUserProfile } from './lib/api'
+
 class App extends React.Component {
   
-  
+  state = {
+    userData: null
+  }
+
+  componentDidMount = () => {
+    const token = localStorage.getItem('token')
+    if (token) this.getUser()
+  }
+
+  getUser = async () => {
+    try {
+      const user = await getUserProfile()
+      this.setState({ userData: user.data })
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
 
   render() {
